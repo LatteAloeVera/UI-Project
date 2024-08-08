276aa1674b6978c6d81cd115790a13ef2da96afd
AloneVera <ayberklove@gmail.com>
2024-08-09 01:30:29 +0300

    Added more UI

--- src/StudentMenuUI.java
+++ src/StudentMenuUI.java
@@ -40,10 +40,11 @@ public class StudentMenuUI extends JFrame {
 	 * Create the frame.
 	 */
 	public StudentMenuUI() {
-		if(currentStudent == null)
-			currentStudentName = "NULL";
-		else
-			currentStudentName = currentStudent.getName();
+		//Non-comment to make easy to open without login
+		//if(currentStudent == null)
+		//	currentStudent = new Student("Selen","uwu","224220002");
+		
+		currentStudentName = currentStudent.getName();
 		setTitle("Student Menu");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 870, 460);
@@ -63,13 +64,6 @@ public class StudentMenuUI extends JFrame {
 		tabbedPane.addTab("Main Menu", null, mainMenuPanel, null);
 		mainMenuPanel.setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel.setBackground(SystemColor.inactiveCaptionBorder);
-		panel.setBounds(418, 11, 398, 350);
-		mainMenuPanel.add(panel);
-		panel.setLayout(null);
-		
 		JPanel infoPanel = new JPanel();
 		infoPanel.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		infoPanel.setBackground(SystemColor.inactiveCaptionBorder);
@@ -77,18 +71,24 @@ public class StudentMenuUI extends JFrame {
 		mainMenuPanel.add(infoPanel);
 		infoPanel.setLayout(null);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
-		panel_1.setBounds(10, 71, 52, 65);
-		infoPanel.add(panel_1);
+		JPanel picturePanel = new JPanel();
+		picturePanel.setBorder(new LineBorder(SystemColor.activeCaption, 2, true));
+		picturePanel.setBounds(20, 71, 52, 65);
+		infoPanel.add(picturePanel);
 		
 		
-		JLabel lblWelcomeNull = new JLabel("= Welcome, "+ currentStudentName + "=");
-		lblWelcomeNull.setBounds(10, 11, 378, 49);
-		infoPanel.add(lblWelcomeNull);
-		lblWelcomeNull.setHorizontalAlignment(SwingConstants.CENTER);
-		lblWelcomeNull.setForeground(new Color(100, 149, 237));
-		lblWelcomeNull.setFont(new Font("Tahoma", Font.BOLD, 19));
+		JLabel lblWelcome = new JLabel("= Welcome, "+ currentStudentName + "=");
+		lblWelcome.setBounds(10, 11, 378, 49);
+		infoPanel.add(lblWelcome);
+		lblWelcome.setHorizontalAlignment(SwingConstants.CENTER);
+		lblWelcome.setForeground(new Color(100, 149, 237));
+		lblWelcome.setFont(new Font("Tahoma", Font.BOLD, 19));
+		
+		JLabel lblUsrNameAndNumber = new JLabel(currentStudentName + ", " + currentStudent.getID() + ".");
+		lblUsrNameAndNumber.setForeground(SystemColor.windowBorder);
+		lblUsrNameAndNumber.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblUsrNameAndNumber.setBounds(82, 78, 133, 14);
+		infoPanel.add(lblUsrNameAndNumber);
 		
 		JPanel panel_1_1 = new JPanel();
 		panel_1_1.setLayout(null);
@@ -96,5 +96,12 @@ public class StudentMenuUI extends JFrame {
 		panel_1_1.setBackground(SystemColor.inactiveCaptionBorder);
 		panel_1_1.setBounds(10, 189, 398, 172);
 		mainMenuPanel.add(panel_1_1);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel.setBackground(SystemColor.inactiveCaptionBorder);
+		panel.setBounds(418, 11, 398, 350);
+		mainMenuPanel.add(panel);
 	}
 }
